1796f419648fc4e94242cbba3b43dde4783a9b38
unknown <grupo3.404@gmail.com>
2022-10-17 20:34:05 -0300

    segundopush

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {FormControl, Validators, FormGroup, FormBuilder} from '@angular/forms';
 
 
 @Component({
@@ -7,12 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+loginform: FormGroup | any;
+  constructor(private _snackBar: MatSnackBar, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-  }
+    this.loginform = this.formBuilder.group({
+      dni: ["", [Validators.required]],
+      password: [
+        "",
+        [
+          Validators.required,
+        ]
+      ]
+    });
 
+  }
+ayuda (
+
+){this._snackBar.open('Pedir contraseña en dirección', 'Cerrar');}
+
+onlogin(){
+
+  alert(this.loginform.value.dni)
+  alert(this.loginform.value.password)
+
+}
 }
 
 
