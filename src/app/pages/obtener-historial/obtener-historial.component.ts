70a5eb2654a7b7ad832ff5106b1508a1b4ef853c
unknown <grupo3.404@gmail.com>
2022-11-04 19:57:48 -0300

    cambiodetabla

--- src/app/pages/obtener-historial/obtener-historial.component.ts
+++ src/app/pages/obtener-historial/obtener-historial.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./obtener-historial.component.scss']
 })
 export class ObtenerHistorialComponent implements OnInit {
-  displayedColumns: string[] = ['name', 'accion', 'symbol','section'];
+  displayedColumns: string[] = ['number','name', 'accion', 'symbol','section'];
   dataSource = ELEMENT_DATA;
   constructor() { }
 
@@ -22,28 +22,17 @@ Component({
   templateUrl: 'table-basic-example.html',
 })
 export interface PeriodicElement {
+  number : number;
   name: string;
   accion: string;
   symbol: string;
   section: string;
 }
 const ELEMENT_DATA: PeriodicElement[] = [
-  { name: 'Taladro(2134341)', accion: 'Disponible' , symbol: '12/04/2022', section:'Nicolas A'},
-  { name: 'Amoladora', accion: 'No Disponible' , symbol: '13/05/2022', section:'Nicolas A'},
-  { name: 'Martillo', accion: 'Disponible' , symbol: '16/06/2022', section:'Nicolas A'},
-  { name: 'Destornillador', accion: 'Disponible' , symbol: '16/07/2022', section:'Nicolas A'},
-  { name: 'Taladro', accion: 'Disponible'  , symbol: '12/04/2022', section:'Nicolas A'},
-  { name: 'Amoladora', accion: 'Disponible' , symbol: '13/05/2022', section:'Nicolas A'},
-  { name: 'Martillo', accion: 'No Disponible' , symbol: '16/06/2022', section:'Nicolas A'},
-  { name: 'Destornillador', accion: 'Disponible' , symbol: '16/07/2022', section:'Nicolas A'},
-  { name: 'Taladro', accion: 'Disponible'  , symbol: '12/04/2022', section:'Nicolas A'},
-  { name: 'Amoladora', accion: ' No Disponible' , symbol: '13/05/2022', section:'Nicolas A'},
-  { name: 'Martillo', accion: 'Disponible' , symbol: '16/06/2022', section:'Nicolas A'},
-  { name: 'Destornillador', accion: 'Disponible' , symbol: '16/07/2022', section:'Nicolas A'},
-  { name: 'Taladro', accion: 'No Disponible'  , symbol: '12/04/2022', section:'Nicolas A'},
-  { name: 'Amoladora', accion: 'Disponible' , symbol: '13/05/2022', section:'Nicolas A'},
-  { name: 'Martillo', accion: 'Disponible' , symbol: '16/06/2022', section:'Nicolas A'},
-  { name: 'Destornillador', accion: 'Disponible' , symbol: '16/07/2022', section:'Nicolas A'},
+  {number: 2221, name: 'Taladro', accion: 'Disponible' , symbol: '12/04/2022', section:'Nicolas A'},
+  {number: 2222, name: 'Amoladora', accion: 'No Disponible' , symbol: '13/05/2022', section:'Nicolas A'},
+  {number: 2223, name: 'Martillo', accion: 'Disponible' , symbol: '16/06/2022', section:'Nicolas A'},
+  {number: 2224, name: 'Destornillador', accion: 'Disponible' , symbol: '16/07/2022', section:'Nicolas A'},
 ];
 
 
