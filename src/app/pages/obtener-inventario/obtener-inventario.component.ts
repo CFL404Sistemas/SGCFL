131fda7da61f3a950e8e4772f05e74f6e57f609c
pc005cfl404 <pc005cfl404@gmail.com>
2022-11-07 17:13:13 -0300

    algo cambio

--- src/app/pages/obtener-inventario/obtener-inventario.component.ts
+++ src/app/pages/obtener-inventario/obtener-inventario.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
+import { filter } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -7,36 +8,52 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './obtener-inventario.component.html',
   styleUrls: ['./obtener-inventario.component.scss'],
 })
-
 export class ObtenerInventarioComponent implements OnInit {
-  todasLasHerramientas:any;
-  herramientasAAsignar: number[]=[];
+  todasLasHerramientas: any;
+  herramientasAAsignar: number[] = [0];
 
-
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit(): void {
-    this.authService.obtenerHerramientas().subscribe((response:any)=>{
-      console.log(response)
-      this.todasLasHerramientas=response.response;
-    })
-
-
+    this.authService.obtenerHerramientas().subscribe((response: any) => {
+      console.log(response);
+      this.todasLasHerramientas = response.response;
+    });
   }
 
-  asignar(herramienta:any){
+  asignar(herramienta: any) {
 
+    const idHerr:number=parseInt(herramienta.id);
+ this.herramientasAAsignar.forEach(element => {
+  element===idHerr? this.herramientasAAsignar.splice(element):this.herramientasAAsignar.unshift(idHerr);
+  element==0?this.herramientasAAsignar.shift():null;
+});
+ console.log(this.herramientasAAsignar)
+
+
+    // if(this.herramientasAAsignar[i]==idHerramienta){
+    //   this.herramientasAAsignar.splice(i,1);
+    //   i--;
+    // }else{
+    //   this.herramientasAAsignar.push(idHerramienta)
+    // }
+
+    //foreach
+    // this.herramientasAAsignar.forEach(element => {
+    //   if(element==idHerramienta){
+    //     this.herramientasAAsignar.splice(element);
+    //   }else{
+    //     this.herramientasAAsignar.push(idHerramienta)
+    //   }
+    // });
   }
 
-  aceptarAsignacion(){
 
+
+
+  aceptarAsignacion() {}
+
+  irAltaHerramienta() {
+    this.router.navigate(['crear-herramienta']);
   }
-
-irAltaHerramienta(){
-  this.router.navigate(["crear-herramienta"]);
-}
-
-
-
-
 }
