a19549558d7fc797f7e5f983de0a129eb68803aa
pc004_CFL404 <pc004_CFL404@gmail.com>
2022-11-28 20:06:36 -0300

    division por rol

--- src/app/pages/home-admin/home-admin.component.ts
+++ src/app/pages/home-admin/home-admin.component.ts
@@ -2,16 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 
 
+
 @Component({
   selector: 'app-home-admin',
   templateUrl: './home-admin.component.html',
   styleUrls: ['./home-admin.component.scss']
 })
 export class HomeAdminComponent implements OnInit {
-
+  rolUsuario: any;
+  datosUsuario: any
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+      //datos del usuario guardado en el localHost
+      this.datosUsuario = JSON.parse(localStorage.getItem('userData')!);
+      //rol del usuario para los ngIf
+      this.rolUsuario = this.datosUsuario.cargo;//this.datosUsuario.cargo;
+
+
+
   }
 
   navegarHerramientas() {
