eefa4f83e663c4a828a38517fc9a8c99e77ec055
porfolio_grupo04 <cfl404@gmail.com>
2022-12-07 19:00:10 -0300

    El cambio mas importante

--- src/app/pages/gracias/gracias.component.ts
+++ src/app/pages/gracias/gracias.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraciasComponent implements OnInit {
 
+  audio: any;
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(){
+
+
   }
 
 }
